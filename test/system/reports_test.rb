f14245b015f9cd45b0192a5c566dd77b122dc298
asahifuke <asahi.fuke@medpeer.co.jp>
2022-06-20 13:05:10 +0900

    作成、更新、削除された後に日報の内容が表示されていることも確認

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -10,6 +10,8 @@ class ReportsTest < ApplicationSystemTestCase
   end
 
   test 'メールアドレスとパスワードでログインして日報を書く' do
+    title = '日報のタイトルの作成'
+    content = '日報の内容の作成'
     sign_out :user
     visit new_user_session_path
     fill_in 'Eメール', with: 'alice@email.com'
@@ -19,31 +21,42 @@ class ReportsTest < ApplicationSystemTestCase
     click_on '日報'
     assert_selector 'h1', text: '日報'
     click_on '新規作成'
-    fill_in 'タイトル', with: 'alice@email.com'
-    fill_in '内容', with: 'password'
+    fill_in 'タイトル', with: title
+    fill_in '内容', with: content
     click_on '登録する'
     assert_text '日報が作成されました。'
+    assert_text title
+    assert_text content
   end
 
   test '日報の編集' do
+    title = '日報のタイトルの更新'
+    content = '日報の内容の更新'
+    alice_report = reports(:alice_report)
     sign_out :user
     sign_in users(:alice)
-    visit reports_path
+    visit report_path(alice_report)
+    assert_no_text title
+    assert_no_text content
     click_on '編集'
-    fill_in 'タイトル', with: '日報のタイトルの更新'
-    fill_in '内容', with: '日報の内容の更新'
+    fill_in 'タイトル', with: title
+    fill_in '内容', with: content
     click_on '更新する'
     assert_text '日報が更新されました。'
+    assert_text title
+    assert_text content
   end
 
   test '日報の削除' do
+    alice_report = reports(:alice_report)
     sign_out :user
     sign_in users(:alice)
     visit reports_path
+    assert_text alice_report.title
     page.accept_confirm do
       click_on '削除', match: :first
     end
-
     assert_text '日報が削除されました。'
+    assert_no_text alice_report.title
   end
 end
