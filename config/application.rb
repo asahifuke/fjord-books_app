284ff82b496536b85cb31c85ec381ba00293e4ee
asahifuke <asahi.fuke@medpeer.co.jp>
2022-06-24 17:11:32 +0900

    デバイスのテストが動くのようにしました

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.assets false  
+      g.skip_routes false
+      g.test_framework :rspec,
+        controller_specs: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end                       
   end
 end
