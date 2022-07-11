52ab7768991da5001d6c8699eb69985d28c4ade0
asahifuke <asahi.fuke@medpeer.co.jp>
2022-07-11 18:09:26 +0900

    rubocopのルールを変更せずに成功させました

--- spec/system/password_spec.rb
+++ spec/system/password_spec.rb
@@ -2,33 +2,8 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
+RSpec.describe 'パスワード変更', type: :system do
   let(:user) { create(:user) }
-
-  it 'メールアドレスとパスワードを使ったログイン' do
-    visit new_user_session_path
-    fill_in 'Eメール', with: user.email
-    fill_in 'パスワード', with: user.password
-    click_on 'ログイン'
-    expect(page).to have_content 'ログインしました'
-  end
-
-  it 'メアドとパスワードでログインするユーザーを2人以上作ることができる。' do
-    visit new_user_registration_path
-    fill_in 'Eメール', with: 'bob@gmail.com'
-    fill_in 'パスワード', with: 'password'
-    fill_in 'パスワード（確認用）', with: 'password'
-    click_on 'サインアップ'
-    expect(page).to have_content 'アカウント登録が完了しました。'
-    click_on 'ログアウト'
-
-    visit new_user_registration_path
-    fill_in 'Eメール', with: 'cavin@gmail.com'
-    fill_in 'パスワード', with: 'password'
-    fill_in 'パスワード（確認用）', with: 'password'
-    click_on 'サインアップ'
-    expect(page).to have_content 'アカウント登録が完了しました。'
-  end
 
   it 'パスワードを変更する場合は、現在のパスワードの入力してプロフィールを編集できるようにする' do
     sign_in user
