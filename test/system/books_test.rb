979dc19a59359319dcfd4863ab37e1d1d891f6ee
asahifuke <asahi.fuke@medpeer.co.jp>
2022-07-12 14:39:38 +0900

    update

---
+++ test/system/books_test.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class BooksTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @book = books(:book1)
+    sign_in users(:alice)
+  end
+
+  test 'visiting the index' do
+    visit books_url
+    assert_selector 'h1', text: '本'
+  end
+
+  test 'creating a Book' do
+    visit books_url
+    click_on '新規作成'
+
+    fill_in 'メモ', with: @book.memo
+    fill_in '題名', with: @book.title
+    click_on '登録する'
+
+    assert_text '本が作成されました。'
+    click_on '戻る'
+  end
+
+  test 'updating a Book' do
+    visit books_url
+    click_on '編集', match: :prefer_exact
+
+    fill_in 'メモ', with: @book.memo
+    fill_in '題名', with: @book.title
+    click_on '更新する'
+
+    assert_text '本が更新されました。'
+    click_on '戻る'
+  end
+
+  test 'destroying a Book' do
+    visit books_url
+    page.accept_confirm do
+      click_on '削除', match: :first
+    end
+
+    assert_text '本が削除されました。'
+  end
+end
